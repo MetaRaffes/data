9847db5df527b2192d2e2b43bbe52fb5bf0ba46a
MetaRaffes <104884956+MetaRaffes@users.noreply.github.com>
2022-05-18 03:55:37 -0400

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,11 +1,58 @@
-contract = new ethers.Contract(address, ["function totalSupply() external view returns (uint256)"], provider)
+const path = require("path");
+const express = require("express");
+const ethers = require("ethers");
 
-If (tokenId > totalSupply) {
-  totalSupply = await contract.totalSupply()
-}
+const contractAddress = "0x799DCdC7D25C78CCd8A52df217EB1E7589A55554";
 
-If (tokenId > totalSupply) {
-  // give error 
-} else {
-  // give data
-}
+let currentSupply = 0;
+
+// Feel free to use whatever provider you want, I usually go with Alchemy
+let socket = new WebSocket("ws://https://yyyyguvbjjy3.usemoralis.com:2053/server")(
+  "Binance Smart Chain", // mainnet, rinkeby, etc
+  "8WSVEQXcrCcGAarH6uWnsb484uSLu3U8gMVOTGNB"
+);
+
+const eventFilter = {
+  address: contractAddress,
+  topics: [
+    // Minting emits a Transfer event from the 0x0 address
+    ethers.utils.id("Transfer(address,address,uint256)"),
+    ethers.utils.hexZeroPad("0x0", 32), 
+  ],
+};
+
+webSocket.on(eventFilter, (event) => {
+    let [name, from, to, tokenIdHex] = event.topics;
+    // All data returned is in hexidecimal, so we need to convert it
+    let tokenId = ethers.BigNumber.from(tokenIdHex).toNumber();
+    if (tokenId > currentSupply) {
+      currentSupply = tokenId;
+      console.log("increased current supply to ", currentSupply);
+    }
+  }
+);
+
+const app = express();
+const metadataDir = path.join(__dirname, "./");
+const imageDir = path.join(__dirname, "./images/");
+const port = 8080;
+
+app.get("/metadata/:id", (req, res) => {
+  if (+req.params.id <= currentSupply) {
+    res.sendFile(metadataDir + req.params.id);
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+app.get("/images/:id", (req, res) => {
+  if (+req.params.id <= currentSupply) {
+    res.sendFile(imageDir + req.params.id + ".png");
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
